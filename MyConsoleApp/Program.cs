0f8f0abe21fecadacc18f894e1604eedf8fa70e4
Kawasaki Taiga <134684996+nhs-tkawasaki@users.noreply.github.com>
2024-06-11 10:33:54 +0900

    Update Program.cs
    result distincted

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -6,21 +6,25 @@ using System.Security.Cryptography;
 // any args : static
 bool flag = args.Length > 0;
 
+const int N = 10;
 Stopwatch sw = new();
 var sha256 = SHA256.Create();
-var queue = new ConcurrentQueue<byte[]>();
+var bag = new ConcurrentBag<byte[]>();
 
 sw.Start();
-Parallel.ForEach(Enumerable.Range(0, 10), i => {
+Parallel.ForEach(Enumerable.Range(0, N), i => {
     byte[] b = { (byte)i };
-    queue.Enqueue(
+    bag.Add(
         flag ? SHA256.HashData(b) : sha256.ComputeHash(b)
     );
 });
 sw.Stop();
 
 Console.WriteLine(flag ? "static" : "instance");
-foreach (var q in queue) {
-    Console.WriteLine(BitConverter.ToString(q));
+;
+var distinct = bag.Select(b => BitConverter.ToString(b)).Distinct().ToArray();
+foreach (var b in distinct) {
+    Console.WriteLine(b);
 }
+if (distinct.Length < N) Console.WriteLine("Duplicated!");
 Console.WriteLine(sw.Elapsed);
