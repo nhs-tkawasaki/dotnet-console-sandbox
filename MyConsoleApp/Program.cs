b878ea19933ec2954b372b4e2fa08898448f60be
Kawasaki Taiga <134684996+nhs-tkawasaki@users.noreply.github.com>
2024-06-12 13:58:17 +0900

    Update Program.cs

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,2 +1,3 @@
 Console.WriteLine("Hello World!");
 const string connectionString = "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+const string dummyApiKey = "d3a3e402dc7f574abe0a1d2d77e4898";
